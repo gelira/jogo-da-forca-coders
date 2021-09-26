13117daa3e18649e3c4906f2346771ce79fd3c3e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-26 14:56:16 -0300

    buscando palavra na API

--- frontend/js/store/actions.js
+++ frontend/js/store/actions.js
@@ -1,3 +1,9 @@
-export default {
+import { ACTIONS, MUTATIONS } from './types.js';
 
+export default {
+  [ACTIONS.FETCH_PALAVRA] (context) {
+    fetch('http://localhost:3000/palavra')
+      .then(response => response.json())
+      .then(data => context.commit(MUTATIONS.SET_PALAVRA, data));
+  }
 };
