817006af1f723fad682b2a25f39cb4493ecb9fe1
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-26 14:54:49 -0300

    pacote de types

--- frontend/js/store/index.js
+++ frontend/js/store/index.js
@@ -2,6 +2,9 @@ import actions from './actions.js';
 import mutations from './mutations.js';
 import state from './state.js';
 import Store from './store.js';
+import { ACTIONS, MUTATIONS } from './types.js';
+
+export { ACTIONS, MUTATIONS };
 
 export default new Store({
   actions,
---
+++ frontend/js/store/types.js
@@ -0,0 +1,7 @@
+export const ACTIONS = {
+  FETCH_PALAVRA: 'FETCH_PALAVRA',
+};
+
+export const MUTATIONS = {
+  SET_PALAVRA: 'SET_PALAVRA',
+};
