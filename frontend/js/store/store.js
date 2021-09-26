905ef5505c72a43bb87811098ce48c87d6db2829
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-26 14:16:22 -0300

    state management

---
+++ frontend/js/store/store.js
@@ -0,0 +1,58 @@
+import PubSub from '../lib/pubsub.js';
+
+export default class Store {
+  constructor(params) {
+    const self = this;
+
+    self.actions = {};
+    self.mutations = {};
+    self.state = {};
+    self.events = new PubSub();
+
+    if (params.hasOwnProperty('actions')) {
+      self.actions = params.actions;
+    }
+
+    if (params.hasOwnProperty('mutations')) {
+      self.mutations = params.mutations;
+    }
+
+    self.state = new Proxy((params.state || {}), {
+      set: function (state, key, value) {
+        state[key] = value;
+        self.events.publish('stateChange', state);
+        
+        return true;
+      }
+    });
+  }
+
+  dispatch(actionKey, payload) {
+    const self = this;
+
+    const func = self.actions[actionKey];
+    if (typeof func !== 'function') {
+      console.error(`Action "${actionKey}" doesn't exist.`);
+      return false;
+    }
+
+    func(self, payload);
+
+    return true;
+  }
+
+  commit(mutationKey, payload) {
+    const self = this;
+
+    const func = self.mutations[mutationKey];
+    if (typeof func !== 'function') {
+      console.log(`Mutation "${mutationKey}" doesn't exist`);
+      return false;
+    }
+
+    const newState = func(self.state, payload);
+    self.state = { ...self.state, ...newState };
+
+    return true;
+  }
+}
