d957141ae424ea48d8ba0fbdccb62d1eb3a58595
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-26 14:55:56 -0300

    tratamento de definição de palavra

--- frontend/js/store/mutations.js
+++ frontend/js/store/mutations.js
@@ -1,3 +1,36 @@
-export default {
+import { MUTATIONS } from './types.js';
 
+export default {
+  [MUTATIONS.SET_PALAVRA] (state, payload) {
+    const { palavra, dica } = payload;
+
+    let letras_restantes = 0;
+    const palavra_masked = [];
+    const palavra_upper = palavra.toUpperCase();
+    const palavra_cleaned = palavra_upper.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+    for (let i = 0; i < palavra.length; i ++) {
+      let masked = true;
+      const letra_upper = palavra_upper[i];
+      const letra_cleaned = palavra_cleaned[i];
+
+      if ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(letra_cleaned)) {
+        letras_restantes ++;
+      }
+      else {
+        masked = false;
+      }
+
+      palavra_masked.push({
+        masked,
+        letra_upper,
+        letra_cleaned,
+      });
+    }
+
+    state.palavra = palavra;
+    state.dica = dica;
+    state.palavra_masked = palavra_masked;
+    state.letras_restantes = letras_restantes;
+  }
 };
