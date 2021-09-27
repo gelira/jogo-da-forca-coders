14ad41384d9c73eb8be25054016b6c74899a670d
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-27 13:48:17 -0300

    refatorando componente palavra

--- frontend/js/components/palavra.js
+++ frontend/js/components/palavra.js
@@ -5,12 +5,16 @@ export default class Palavra extends Component {
     super();
     this.elementDica = document.getElementById('dica');
     this.elementPalavraMasked = document.getElementById('palavra-masked');
+    this.elementLetrasRestantes = document.getElementById('letras-restantes');
   }
 
-  render() {
-    const { dica, palavra_masked } = this.store.state;
+  showDica() {
+    this.elementDica.innerHTML = this.store.state.dica;
+  }
 
-    this.elementDica.innerHTML = dica;
+  renderPalavraMasked() {
+    const { palavra_masked } = this.store.state;
+
     this.elementPalavraMasked.innerHTML = '';
 
     for (let i = 0; i < palavra_masked.length; i ++) {
@@ -27,5 +31,23 @@ export default class Palavra extends Component {
 
       this.elementPalavraMasked.appendChild(div);
     }
+  }
+
+  renderLetrasRestantes() {
+    const { letras_restantes } = this.store.state;
+
+    if (letras_restantes <= 0) {
+      this.elementLetrasRestantes.classList.add('hidden');
+      return;
+    }
+
+    this.elementLetrasRestantes.classList.remove('hidden');
+    this.elementLetrasRestantes.innerHTML = letras_restantes === 1 ? 'Falta apenas 1 letra' : `Faltam ${letras_restantes} letras`;
+  }
+
+  render() {
+    this.showDica();
+    this.renderPalavraMasked();
+    this.renderLetrasRestantes();
   }
 }
