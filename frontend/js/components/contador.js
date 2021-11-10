3bd738bcb41d9416f3c5d5789cdea2ff7f596e10
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-10 20:47:39 -0300

    componente de contador

---
+++ frontend/js/components/contador.js
@@ -0,0 +1,25 @@
+import Component from '../lib/component.js';
+
+export default class Contador extends Component {
+  constructor() {
+    super();
+    this.elementContador = document.querySelector('#contador');
+  }
+
+  subscribe() {
+    this.store.events.subscribe('tempoChange', () => this.render());
+  }
+
+  render() {
+    const { tempo } = this.store.state;
+    console.log(tempo);
+
+    const segundos = tempo % 60;
+    const minutos = (tempo - segundos) / 60;
+
+    const segundosText = segundos.toString().padStart(2, '0');
+    const minutosText = minutos.toString().padStart(2, '0');
+
+    this.elementContador.innerHTML = `Tempo de jogo: ${minutosText}:${segundosText}`;
+  }
+}
