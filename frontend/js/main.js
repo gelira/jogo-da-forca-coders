c6936bab59e7bf65fa0f0facb45d8b1d89778647
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-11-10 20:49:45 -0300

    iniciando contador e action de tempo

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,23 @@
 import Palavra from './components/palavra.js';
 import Resultado from './components/resultado.js';
-import Tentativa from './components/tentativa.js'
+import Tentativa from './components/tentativa.js';
+import Contador from './components/contador.js';
+
+import store from './store/index.js';
+import { ACTIONS } from './store/types.js';
 
 window.addEventListener('load', () => {
+  store.dispatch(ACTIONS.INICIAR_TEMPO, { tempo: 0 });
+
   const palavra = new Palavra();
   const tentativa = new Tentativa();
   const resultado = new Resultado();
+  const contador = new Contador();
 
   resultado.novoJogo();
 
   palavra.render();
   tentativa.render();
   resultado.render();
+  contador.render();
 });
